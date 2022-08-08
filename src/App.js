92bee8f5c2e2fad98a88c122ef8bf59399ef9cc0
Akshay Singh <akshay1119@outlook.com>
2022-08-08 21:31:10 +0530

    Budget app complete without Redux.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,148 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
-import EntryLine from './components/EntryLine';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
+import EntryLines from './components/EntryLines';
+import ModalEdit from './components/ModalEdit';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [isExpense, setIsExpense] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [entryId, setEntryId] = useState();
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if(!isOpen && entryId){
+      const index = entries.findIndex(entry => entry.id === entryId);
+      const newEntries = [...entries];
+      newEntries[index].description = description;
+      newEntries[index].value = value;
+      newEntries[index].isExpense = isExpense;
+      setEntries(newEntries);
+      resetEntry();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[isOpen])
+
+  useEffect(() => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+    entries.map(entry => {
+      if(entry.isExpense) {
+        return (totalExpense += Number(entry.value));
+      }
+      return (totalIncome += Number(entry.value));    
+    });
+    setTotal(totalIncome - totalExpense);
+    setIncomeTotal(totalIncome);
+    setExpenseTotal(totalExpense);
+  }, [entries])
+
+  const deleteEntry = id => {
+    const result = entries.filter(entry => entry.id !== id);
+    setEntries(result);
+  }
+
+  const editEntry = id => {
+    if(id){
+      const index = entries.findIndex(entry => entry.id === id);
+      const entry = entries[index];
+      setEntryId(entry.id)
+      setDescription(entry.description);
+      setValue(entry.value);
+      setIsExpense(entry.isExpense);
+      setIsOpen(true);
+    }
+  }
+
+  const addEntry = () => {
+    const result = entries.concat({id: entries.length+1,
+       description,
+       value,
+       isExpense
+      });
+    setEntries(result);
+    resetEntry();
+  }
+
+  function resetEntry() {
+    setDescription('');
+    setValue('');
+    setIsExpense(true);
+  }
+
   return (
     <Container>
-
         <MainHeader title='Budget' />
-        <DisplayBalance title='Your Balance' value='2,550.53' size='small' />
+        <DisplayBalance title='Your Balance' value={total} size='small' />
         
-        <DisplayBalances />
+        <DisplayBalances totalIncome={incomeTotal} totalExpense={expenseTotal}/>
 
         <MainHeader title='History' type='h3'/>
-        <EntryLine description="Somthing new" value='1000' color='green' />
-        <EntryLine description="Somthing new" value='1000' color='red' />
-        <EntryLine description="Somthing new" value='1000' color='green' />
+        <EntryLines 
+          entries={entries}
+          deleteEntry={deleteEntry}
+          editEntry={editEntry}
+        />
 
         <MainHeader title='Add new transaction' type='h3'/>
-        <NewEntryForm />
-
+        <NewEntryForm 
+          addEntry={addEntry}
+          description={description}
+          value={value}
+          isExpense={isExpense}
+          setDescription={setDescription}
+          setValue={setValue}
+          setIsExpense={setIsExpense}
+        />
+        <ModalEdit 
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          addEntry={addEntry}
+          description={description}
+          value={value}
+          isExpense={isExpense}
+          setDescription={setDescription}
+          setValue={setValue}
+          setIsExpense={setIsExpense}
+        />
     </Container>
   );
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    id: 1,
+    description: "Work income",
+    value: 300,
+    isExpense: false
+  },
+  {
+    id: 2,
+    description: "Water bill",
+    value: 300,
+    isExpense: true
+  },
+  {
+    id: 3,
+    description: "Rent",
+    value: 300,
+    isExpense: true
+  },
+  {
+    id: 4,
+    description: "Power Bill",
+    value: 300,
+    isExpense: true
+  },
+]
--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Segment, Grid, Icon } from 'semantic-ui-react';
 
-function EntryLine({ description, value, color }) {
+function EntryLine({ id, description, value, isExpense = false, deleteEntry, editEntry }) {
     return(
-        <Segment color={color}>
+      <Fragment>
+        <Segment color={isExpense ? 'red' : 'green'}>
           <Grid columns={3} textAlign='right'>
             <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>${description}</Grid.Column>
+            <Grid.Column width={3} textAlign='right'>${value}</Grid.Column>
             <Grid.Column width={3}>
-              <Icon name='edit'></Icon>
-              <Icon name='trash'></Icon>
+              <Icon name='edit' bordered onClick={() => editEntry(id)}></Icon>
+              <Icon name='trash' bordered onClick={() => deleteEntry(id)}></Icon>
             </Grid.Column>
           </Grid>
-        </Segment>
+        </Segment>        
+      </Fragment>
     )
 }
 
