ad89ac108dc79136081e401f29f6b5484b381350
Akshay Singh <akshay1119@outlook.com>
2022-08-14 20:16:04 +0530

    Saga implimentation w/o redux.

---
+++ src/sagas/testSaga.js
@@ -0,0 +1,24 @@
+import { take, delay, put, call } from 'redux-saga/effects';
+
+function double(number){
+    return number * 2;
+}
+
+export function* testSaga(){
+    while (true) {
+        console.log("starting saga"); 
+        const state = yield take('TAKE_MASSAGE');
+        const a = yield call(double, 2);
+        console.log("call", a);
+        const b = yield double(3);
+        console.log("no call", b);
+        console.log("finishing saga", state);
+    }
+}
+
+export function* dispatchTest(){
+    while(true){
+        yield delay(1000);
+        yield put({ type: 'TAKE_MASSAGE', payload: 1000 });
+    }
+}
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,16 +1,24 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import entriesReducer from '../reducers/entries.reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import modalsReducer from '../reducers/modals.reducer';
+import createSagaMiddleware from "@redux-saga/core";
+import { initSagas } from "../sagas";
 
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 const storeConfig = () => {
-    return createStore(
+    const store = createStore(
         combineReducers({
             entries: entriesReducer,
             modals: modalsReducer
         }),
-        composeWithDevTools()
+        composeWithDevTools(
+            applyMiddleware(...middlewares))
     );
+    initSagas(sagaMiddleware);
+    
+    return store;
 };
 
 export default storeConfig;
